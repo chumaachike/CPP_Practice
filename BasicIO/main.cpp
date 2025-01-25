a49334df81b72a369c70bd64884e79cf667b0aca
chumaachike <edwardachike@gmail.com>
2025-01-25 12:57:01 +0000

    basic io working

---
+++ BasicIO/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    // cout << "Hello world!";
+    
+    int num1;
+    int num2;
+    double num3;
+    
+//    cout << "Hello";
+//    cout << "World" << endl;
+
+    cout << "Enter an integer";
+    cin >> num1;
+    cout << "You entered: " << num1 << endl;
+    
+}
